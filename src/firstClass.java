abaff7e4ea7b2475f6caf073df686611f1e40b20
Aruuke <akydyralieva06@gmail.com>
2019-11-09 14:16:54 -0600

    Second try

--- src/firstClass.java
+++ src/firstClass.java
@@ -1,6 +1,6 @@
 public class firstClass {
     public static void main(String[] args) {
         System.out.println("First line");
-        System.out.println("Second line");
+        System.out.println("Second line ghhj");
     }
 }
