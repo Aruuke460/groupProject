4244835d0b360afd581dd82dc7f40c25c9de2547
Aruuke <akydyralieva06@gmail.com>
2019-11-09 14:51:47 -0600

    second push

---
+++ src/groupPractice.java
@@ -0,0 +1,8 @@
+public class groupPractice {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        if(true) {
+            System.out.println("hello branch2");
+        }
+    }
+}
